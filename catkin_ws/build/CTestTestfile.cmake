cc5067bb732561d4c1193bed1e26305c180ba559
pascal <davidxiang101@gmail.com>
2024-07-02 10:29:24 -0400

    moved workspace correctly

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/pascal/catkin_ws/src
+# Build directory: /home/pascal/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("autoturtle")
+subdirs("beginner_tutorials")
